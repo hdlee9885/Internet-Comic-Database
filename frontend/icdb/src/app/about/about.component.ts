a9c843806ee6b709841464dcad3e1f14f5dc5666
Haosong Li <hdlee9885@wireless-10-145-252-127.public.utexas.edu>
2020-03-06 23:14:19 -0600

    Everything we have for Phase I

--- frontend/icdb/src/app/about/about.component.ts
+++ frontend/icdb/src/app/about/about.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
 
-  constructor() { }
+@Injectable()
+export class AboutComponent implements OnInit {
+  github_json;
+  github_issue;
+  total;
+  author = [];
+  jacob;
+  chris;
+  jerad;
+  william;
+  haosong;
+  josh;
+  issue;
+  numOfIssue;
+  
+  headerDict = {
+	  'Content-Type': 'application/json',
+	  'Accept': 'application/json',
+	  'Access-Control-Allow-Headers': 'Content-Type',	  
+	  'Access-Control-Allow-Origin': '*',  
+	}
+	requestOptions = {                                                                                                                                                                                 
+	  headers: new HttpHeaders(this.headerDict), 
+	};
+  
+  
+  constructor(private http: HttpClient) { 
+  }
 
   ngOnInit(): void {
+     this.github_json = this.http.get<any>('https://api.github.com/repos/chrisjoswin/EE461L_Project/stats/contributors').subscribe(data => {
+     	this.jacob = data[0];
+     	console.log(this.jacob);
+		this.chris = data[1];
+		this.jerad = data[2];
+		this.william = data[3];
+		this.haosong = data[4];
+		this.josh = data[5];
+		     	
+		     	
+     });
+ 	 this.github_issue = this.http.get<any>('https://api.github.com/repos/chrisjoswin/EE461L_Project/issues').subscribe(data => {
+ 	 	this.numOfIssue = data[0];
+ 	 });
+  }
+  
+  ngAfterViewChecked(){
+  	console.log(this.github_json);
   }
 
 }
