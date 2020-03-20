9a2a703abc5aa77aa32c5eb203fbf4e6ca0b3976
j-ka11 <joshuakall@utexas.edu>
2020-03-19 19:35:57 -0500

    cleaned up about component

--- frontend/icdb/src/app/about/about.component.ts
+++ frontend/icdb/src/app/about/about.component.ts
@@ -23,24 +23,6 @@ export class AboutComponent implements OnInit {
     {name: 'William Gu', photoUrl: this.blankPhoto, bio: '', track: '', team: 'Backend Team', numIssues: 0, numCommits: 0, numTests: 0},
   ];
 
-  commits = {
-    jacob: 0,
-    chris: 0,
-    jerad: 0,
-    william: 0,
-    haosong: 0,
-    josh: 0
-  };
-
-  issues = {
-    jacob: 0,
-    chris: 0,
-    jerad: 0,
-    william: 0,
-    haosong: 0,
-    josh: 0
-  };
-
   numCommits = 0;
   numIssues = 0;
 
@@ -51,27 +33,21 @@ export class AboutComponent implements OnInit {
         this.numCommits += contributor.total;
         switch (contributor.author.login) {
           case 'jacobgrimm':
-            this.commits.jacob = contributor.total;
             this.developers[3].numCommits = contributor.total;
             break;
           case 'Minalinnski':
-            this.commits.william = contributor.total;
             this.developers[5].numCommits = contributor.total;
             break;
           case 'chrisjoswin':
-            this.commits.chris = contributor.total;
             this.developers[0].numCommits = contributor.total;
             break;
           case 'JSRobles':
-            this.commits.jerad = contributor.total;
             this.developers[4].numCommits = contributor.total;
             break;
           case 'hdlee9885':
-            this.commits.haosong = contributor.total;
             this.developers[2].numCommits = contributor.total;
             break;
           case 'j-ka11':
-            this.commits.josh = contributor.total;
             this.developers[1].numCommits = contributor.total;
             break;
         }
@@ -87,27 +63,21 @@ export class AboutComponent implements OnInit {
         this.numIssues++;
         switch (issue.user.login) {
           case 'jacobgrimm':
-            this.issues.jacob++;
             this.developers[3].numIssues++;
             break;
           case 'Minalinnski':
-            this.issues.william++;
             this.developers[5].numIssues++;
             break;
           case 'chrisjoswin':
-            this.issues.chris++;
             this.developers[0].numIssues++;
             break;
           case 'JSRobles':
-            this.issues.jerad++;
             this.developers[4].numIssues++;
             break;
           case 'hdlee9885':
-            this.issues.haosong++;
             this.developers[2].numIssues++;
             break;
           case 'j-ka11':
-            this.issues.josh++;
             this.developers[0].numIssues++;
             break;
         }
