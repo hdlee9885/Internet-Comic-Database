0336e54a6cdabb17b9b69b7bda58de8d8e9bf711
hdlee9885 <44150072+hdlee9885@users.noreply.github.com>
2020-04-24 21:43:19 -0500

    Phase III Web Finalized

--- frontend/icdb/src/app/about/about.component.ts
+++ frontend/icdb/src/app/about/about.component.ts
@@ -16,7 +16,9 @@ export class AboutComponent implements OnInit {
   JacobPhoto = '../../assets/photo/Jacob.jpg';
   JoshPhoto = '../../assets/photo/Josh.PNG';
   ChrisPhoto = '../../assets/photo/Chris.jpg';
-  HaosongPhoto = '../../assets/photo/Haosong.jpeg'
+  HaosongPhoto = '../../assets/photo/Haosong.jpeg';
+  WillPhoto = '../../assets/photo/William.png';
+  SebPhoto = '../../assets/photo/Seb.JPG';
 
   developers: Developer[] = [
     // tslint:disable-next-line:max-line-length
@@ -24,8 +26,8 @@ export class AboutComponent implements OnInit {
     {name: 'Josh Kall', photoUrl: this.JoshPhoto, bio: 'Josh is currently a Junior at UT Austin, he likes to cook, and likes to play Smash. He will be interning at Google in Summer 2020.', track: 'Software Engineering and Design', team: 'Frontend Team', numIssues: 0, numCommits: 0, numTests: 29},
     {name: 'Haosong Li', photoUrl: this.HaosongPhoto, bio: 'Haosong is a Senior ECE student at UT on software track. He likes traveling during holidays and likes playing basketball.', track: 'Software Engineering and Design', team: 'Frontend Team', numIssues: 0, numCommits: 0, numTests: 24},
     {name: 'Jacob Grimm', photoUrl: this.JacobPhoto, bio: 'Jacob is currently a Junior at UT Austin, he is super cool and likes to play Fortnite.', track: 'Software Engineering and Design', team: 'Backend Team', numIssues: 0, numCommits: 0, numTests: 5},
-    {name: 'Sebastian Robles', photoUrl: this.blankPhoto, bio: 'Sebastian is currently a Senior at UT Austin, an active member of the Maker movement and a huge fan of Star Wars and comics. He will begin working for Visa Inc. in September.', track: 'Embedded Systems and Computer Architecture', team: 'Backend Team', numIssues: 0, numCommits: 0, numTests: 5},
-    {name: 'William Gu', photoUrl: this.blankPhoto, bio: 'Chris is a Junior at UT studying ECE with his tech core as Software Design. In his free time he likes going outside and watching sports.', track: 'Software Engineering and Design', team: 'Backend Team', numIssues: 0, numCommits: 0, numTests: 6},
+    {name: 'Sebastian Robles', photoUrl: this.SebPhoto, bio: 'Sebastian is currently a Senior at UT Austin, an active member of the Maker movement and a huge fan of Star Wars and comics. He will begin working for Visa Inc. in September.', track: 'Embedded Systems and Computer Architecture', team: 'Backend Team', numIssues: 0, numCommits: 0, numTests: 5},
+    {name: 'William Gu', photoUrl: this.WillPhoto, bio: 'William is currently a junior student at UT Austin, he is a great fan of anime and League of Legend.', track: 'Software Engineering and Design', team: 'Backend Team', numIssues: 0, numCommits: 0, numTests: 6},
   ];
 
   numCommits = 0;
